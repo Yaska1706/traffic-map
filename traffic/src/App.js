db631d6e4c80a56da4cbbf392437ffa337294716
yaska1706 <yasinkinyua@gmail.com>
2020-04-18 03:53:06 +0300

    Separated header components

--- traffic/src/App.js
+++ traffic/src/App.js
@@ -1,50 +1,21 @@
 import React from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from 'reactstrap';
 import './App.css';
+import Header from './components/Header'
 
 class App extends React.Component {
 
   constructor(props){
     super(props);
 
-    this.toggle = this.toggle.bind(this);
     this.state = {
-      isOpen:false
+      applicationName: "TrafficMap"
     }
   }
-  toggle(){
-    this.setState ({ 
-      isOpen: !this.state.isOpen
-    });
-  }
+  
   render(){
   return (
     <div className="App">
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Traffic</NavbarBrand>
-        <NavbarToggler onClick={this.toggle.bind(this)} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              100 traffic incidents
-            </NavItem>
-            
-          </Nav>
-        </Collapse>
-      </Navbar> 
+      <Header appName={this.state.applicationName}/>
     </div>
   );
   }
